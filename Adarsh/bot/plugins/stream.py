8a7bc13a87a8141eee72a843222ac119fc8136cd
patriotes <154737887+patriotes@users.noreply.github.com>
2024-01-31 20:57:20 +0100

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -35,7 +35,7 @@ async def private_receive_handler(c: Client, m: Message):
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
        
-        msg_text ="""<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥WATCH  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : LINK WON'T EXPIRE TILL I DELETE</b>"""
+        msg_text ="""<i><u>𝗩𝗼𝘁𝗿𝗲 𝗟𝗶𝗲𝗻 𝗘𝘀𝘁 𝗣𝗿𝗲𝘁 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 🅣🅔🅛🅔🅒🅗🅐🅡🅖🅔🅡 :</b> <i>{}</i>\n\n<b> 🖥🅡🅔🅖🅐🅡🅓🅔🅡  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : 𝐋𝐄 𝐋𝐈𝐄𝐍 𝐍'𝐄𝐗𝐏𝐈𝐑𝐄 𝐏𝐀𝐒 𝐓𝐄𝐌𝐏𝐒 𝐐𝐔𝐄 𝐉𝐄 𝐍𝐄 𝐋'𝐄𝐅𝐅𝐀𝐂𝐄 𝐏𝐀𝐒</b>"""
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
